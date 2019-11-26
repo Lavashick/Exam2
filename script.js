432aed4d2d8718b238d4f4e89c4ec5d16e505549
Lavashick <lavashick@icloud.com>
2019-11-26 17:21:36 +0300

    Функция добавления в таблицу

--- script.js
+++ script.js
@@ -8,25 +8,43 @@ function find(id) {
     return document.getElementById(id);
 }
 
+// Тело таблицы
+var table = find("main-table").getElementsByTagName("tbody");
+
+
 
 // Валидация ввода и взятие данных из формы
 find("show-table-btn").addEventListener("click", function () {
     date = find("date").value;
     description = find("description").value;
     if (!(date === "" & description === ""))
-        add_to_table(date, description);
+        add_to_table_array(date, description);
 });
 
 // Запись данных в массив для таблицы
-function add_to_table(date, description) {
+function add_to_table_array(date, description) {
     number += 1;
     arr_table[0] = number;
     arr_table[1] = date;
     arr_table[2] = description;
-    alert(arr_table[2]);
+    add_to_table(arr_table);
 }
 
 
+var createTr = document.createElement("tr");
+var createTh = document.createElement("th");
+var createTd = document.createElement("td");
+
+
+
+function add_to_table(array) {
+    var new_row = document.createElement('tr');
+    var new_col = new_row.createElement('td');
+    new_col.innerText = array[0];
+    new_col.innerText = array[1];
+    new_col.innerText = array[2];
+    table.appendChild(new_row);
+}
 
 
 
@@ -40,7 +58,7 @@ function add_to_table(date, description) {
                 event.preventDefault();
                 event.stopPropagation();
                 if (form.checkValidity() === true) {
-                    show_info();
+                    show_info()
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -49,21 +67,3 @@ function add_to_table(date, description) {
 })();
 
 
-
-
-// Валидация на правильность введенного текста
-/*(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-        var forms = document.getElementsByClassName('needs-validation');
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-                event.preventDefault();
-                event.stopPropagation();
-                if (form.checkValidity() === true)
-                    show_info()
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();*/
