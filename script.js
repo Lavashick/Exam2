88e0ef67cee8ceeebcb1b59366ed762982a30458
Lavashick <lavashick@icloud.com>
2019-11-26 19:45:20 +0300

    ldshvren

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var date; // Дата инцидента
 var description; // Описание инцидента
 var arr_table = [];
 var sum_row = 0;
+var all_table_values;
 
 // Поиск элемента по id
 function find(id) {
@@ -38,6 +39,8 @@ function add_to_table_array(date, description) {
     arr_table[1] = date;
     arr_table[2] = description;
     add_to_table(arr_table);
+
+
 }
 
 // Тело таблицы
@@ -48,12 +51,14 @@ function add_to_table(arr) {
     var text_html = "<td>" + arr[0] + "</td><td>" + arr[1] + "</td><td>" + arr[2] + "</td>" + "<td style='cursor: pointer; color: red;' onclick='delete_row()'>&#10008</td>";
     var new_row = document.createElement('tr');
     new_row.innerHTML = text_html;
+    new_row.id = "number" + arr[0];
+    new_row.
     table.appendChild(new_row);
     update_sum_row();
 }
 
 function delete_row() {
-    
+
 }
 
 
@@ -73,3 +78,61 @@ function delete_row() {
         });
     }, false);
 })();
+
+/*
+
+// Запись данных в массив для таблицы
+function add_to_table_array(date, description) {
+    number += 1;
+    arr_table.push([number, date, description]);
+    add_to_table(arr_table[number - 1]);
+}
+*/
+/*
+// Тело таблицы
+var table = find("table-body");
+
+function add_to_table(data) { // Просто добавление новых данных
+    let row_index = table.rows.length;
+    let row = table.insertRow(row_index); // Создание новой строчки
+    for (let i = 0; i < data.length; i++) {
+        let cell = row.insertCell(i); // Создание новой ячейки
+        cell.innerText = data[i]; // Добавление данных в нее
+    }
+    let cell = row.insertCell(data.length);
+    cell.innerHTML = "&#10008;";
+    cell.style.color = "red";
+    cell.addEventListener("click", function () { remove_from_table(row); });
+    // Данные генерируются в таблице в реальном времени, так как все работает через ссылки (getElementById возвращает ссылку,
+    // insertCell возвращает ссылку на объект и тд
+    update_sum_row();
+}
+
+function remove_from_table(row) {
+    let index = row.rowIndex - 1;
+    arr_table.splice(index, 1);
+    table.deleteRow(index);
+    update_sum_row();
+}
+
+
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                if (form.checkValidity() === true) {
+                    var date = find("date").value;
+                    var description = find("description").value;
+                    add_to_table_array(date, description);
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+ */
